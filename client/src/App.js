649297479cda32438edb8367ab0fb326b4ed55d0
gitcheegoo <henrylee2101@gmail.com>
2023-08-09 13:40:24 +1000

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
       {" "}
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Footer />
     </div>
   );
 }
