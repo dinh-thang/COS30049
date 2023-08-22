ff9afb8ddf2e744d803a6b550d53aaccccc9c0c4
ok-bong <kaybgmp1607@gmail.com>
2023-08-23 00:05:37 +1000

    Add sample data for reports

--- client/src/pages/Report.jsx
+++ client/src/pages/Report.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Search from "../components/Search";
+// This is a sample hard-coded data for the report page, the data here is for demo purpose and the vulnerability data are taken from Slither documentation.
+// Reference: https://github.com/crytic/slither/wiki/Detector-Documentation
+import reportData from "../assets/reportData.json";
 
 const Report = () => {
   return (
     <div>
+      <h1>Report Page</h1>
       <Search />
+      {/* use map() function + styling nha */}
     </div>
   );
 };
